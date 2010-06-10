bede645706a8e1a755b27f1dfdb0ab19c78afed9
Your Name <evulfson@gmail.com>
2010-06-09 22:00:14 -0400

    depot chapter 9 less some ajax

--- HW/HW3/depot/app/controllers/store_controller.rb
+++ HW/HW3/depot/app/controllers/store_controller.rb
@@ -1,7 +1,68 @@
 class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
+    @cart = find_cart
     @time = Time.now
+    @count = increment_count		# How many times has index been accessed
+  end
+
+  def add_to_cart
+    product = Product.find(params[:id])
+    @cart = find_cart
+    @current_item = @cart.add_product(product)
+    @count = session[:counter] = 0 #reset counter
+    respond_to do |format|
+      format.js if request.xhr?
+      format.html {redirect_to_index} #why does this not invoke regardless of format.js line's result (one above)?
+    end
+  rescue ActiveRecord::RecordNotFound
+    logger.error("Attempt to access invalid product #{params[:id]}")
+    redirect_to_index("Invalid product")
+  end
+
+  def subtract_from_cart
+    begin
+      product = Product.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error("Attempt to access invalid product #{params[:id]}")
+      redirect_to_index("Invalid product")
+    else
+      @cart = find_cart
+      @current_item = @cart.subtract_product(product)
+
+      respond_to do |format|
+        format.js if request.xhr?
+        format.html {redirect_to_index} #why does this not invoke regardless of format.js line's result (one above)?
+      end
+    end
+    # redirect_to_index unless request.xhr?
+
+    
+  end
+
+
+  def empty_cart
+    session[:cart] = nil #doesn't this belong in the model?
+    respond_to do |format|
+      format.js if request.xhr?
+      format.html {redirect_to_index}
+    end
+  end
+
+  private
+
+  def redirect_to_index(msg = nil)
+    flash[:notice] = msg if msg
+    redirect_to :action => 'index'
+  end
+
+  def increment_count
+    session[:counter] ||= 0 
+    session[:counter] += 1
+  end
+
+  def find_cart
+    session[:cart] ||= Cart.new	# return existing or new cart
   end
 
 end
--- HW/HW3/depot/app/models/product.rb
+++ HW/HW3/depot/app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ActiveRecord::Base
 
-
   def self.find_products_for_sale
     find(:all, :order => "title")
   end
