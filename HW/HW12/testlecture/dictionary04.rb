04ad018c53aaa43d7b139c621858b5a0191d3024
Your Name <evulfson@gmail.com>
2010-06-22 12:51:55 -0400

    finished dictionary05 and moved it all to HW12

--- HW/HW12/testlecture/dictionary04.rb
+++ HW/HW12/testlecture/dictionary04.rb
@@ -1,10 +1,4 @@
 require 'test/unit'
-require 'rubygems'
-require 'mocha'
-
-module Exceptions
-  class MissingWordException < StandardError; end
-end
 
 # Design a class to represent a dictionary
 class Dictionary
@@ -24,7 +18,6 @@ class Dictionary
   end
   
   def translate(otherlanguage)
-    raise Exceptions::MissingWordException if @dictionary[otherlanguage].nil? #HW: Raises an exception
     @dictionary[otherlanguage]
   end
 
@@ -62,37 +55,6 @@ class DictionaryTest < Test::Unit::TestCase
     assert !@dict.empty?
     assert_equal "book", @dict.translate("boek")
     assert_equal "house", @dict.translate("huis")
-  end
-
-# Let's check that fetching a word that doesn't exist throws an exception
-#  def test_fetch_missing_word
-#    @dict.add_translation("book", "boek")
-#    
-#  end
-
-# What if the dictionary is empty?
-  def test_fetch_missing_word_from_empty_dictionary
-    missing_word = "asdf"
-    assert_raises Exceptions::MissingWordException do # HW: Checks for an exception!
-      @dict.translate(missing_word)
-    end
-  end  
-end
-# This mocks functionality of dictionary which is still not implemented. For instance, translating words from non-english, back to english, not the other way
-class MockDictionaryTest < Test::Unit::TestCase
-  
-  def test_the_truth
-    assert true
-  end
-
-  # You see, the reverse translate method doesn't actually exist, but that's where mock comes in!
-  # test_mocking_an_instance_method_on_a_real_object
-  def test_reverse_translate
-    dict = Dictionary.new
-    word_ru = "slova"
-    word_en = "word"
-    dict.expects(:reverse_translate).with(word_ru).returns(word_en)
-    assert_equal word_en, dict.reverse_translate(word_ru)
   end
   
 end
