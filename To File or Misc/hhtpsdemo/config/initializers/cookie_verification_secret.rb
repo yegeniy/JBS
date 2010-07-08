015913b14f5e1d6c8cdbdd8e3fe6c647d627beef
Your Name <evulfson@gmail.com>
2010-07-08 01:15:43 -0400

    commit some work... hope I didnt lose any

---
+++ To File or Misc/hhtpsdemo/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'f7829e9c94c262f5dae7820b82191770636710296c1ec7de6fee2f3522409b48b1cf8c37d2ec740f98d92a8369f2d2a46d6637fe6e64a58097a7e35a113e0023';
